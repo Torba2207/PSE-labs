2d7f707b1e9a990655b75da253fe5528d059ff10
Torba2207 <alexnychyporchuk@gmail.com>
2025-03-11 18:01:49 +0100

    init:Lab2

---
+++ Lab2/Lab2.srcs/sources_1/new/top.vhd
@@ -0,0 +1,49 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/11/2025 04:54:20 PM
+-- Design Name: 
+-- Module Name: top - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity top is
+
+    Port ( clk_i : in STD_LOGIC;
+
+           rst_i : in STD_LOGIC;
+
+           led_o : out STD_LOGIC_VECTOR (2 downto 0));
+
+end top;
+
+architecture Behavioral of top is
+
+begin
+
+
+end Behavioral;
