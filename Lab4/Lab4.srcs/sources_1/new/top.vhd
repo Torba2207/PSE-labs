13268162642d78c21121e20fc4227b34017a6ff5
Torba2207 <alexnychyporchuk@gmail.com>
2025-03-25 19:41:01 +0100

    add: Top with display and receiver moduls has been implemented

--- Lab4/Lab4.srcs/sources_1/new/top.vhd
+++ Lab4/Lab4.srcs/sources_1/new/top.vhd
@@ -1,43 +1,62 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 03/25/2025 03:20:42 PM
--- Design Name: 
--- Module Name: top - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
+-- Top-Level Module: RS232 Receiver with 7-Segment Display
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+entity Top is
+    Port (
+        clk_i : in STD_LOGIC;              
+        rst_i : in STD_LOGIC;              
+        RXD_i : in STD_LOGIC;              
+        led7_an_o : out STD_LOGIC_VECTOR(3 downto 0); 
+        led7_seg_o : out STD_LOGIC_VECTOR(7 downto 0) 
+    );
+end Top;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+architecture Behavioral of Top is
+    component RS232_Receiver is
+        Port (
+            clk_i : in STD_LOGIC;
+            rst_i : in STD_LOGIC;
+            RXD_i : in STD_LOGIC;
+            data_o : out STD_LOGIC_VECTOR(7 downto 0);
+            data_ready_o : out STD_LOGIC
+        );
+    end component;
 
-entity top is
---  Port ( );
-end top;
+    component Display_Controller is
+        Port (
+            clk_i : in STD_LOGIC;
+            rst_i : in STD_LOGIC;
+            data_i : in STD_LOGIC_VECTOR(7 downto 0);
+            led7_an_o : out STD_LOGIC_VECTOR(3 downto 0);
+            led7_seg_o : out STD_LOGIC_VECTOR(7 downto 0)
+        );
+    end component;
 
-architecture Behavioral of top is
+    -- Signals
+    signal data_ready : STD_LOGIC;
+    signal data_reg : STD_LOGIC_VECTOR(7 downto 0);
 
 begin
+    
+    RS232_Receiver_Inst : RS232_Receiver
+        Port map (
+            clk_i => clk_i,
+            rst_i => rst_i,
+            RXD_i => RXD_i,
+            data_o => data_reg,
+            data_ready_o => data_ready
+        );
 
+    
+    Display_Controller_Inst : Display_Controller
+        Port map (
+            clk_i => clk_i,
+            rst_i => rst_i,
+            data_i => data_reg,
+            led7_an_o => led7_an_o,
+            led7_seg_o => led7_seg_o
+        );
 
 end Behavioral;
